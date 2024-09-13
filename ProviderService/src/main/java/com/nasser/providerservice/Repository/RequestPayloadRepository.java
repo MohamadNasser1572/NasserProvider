a689a1b9479d886914c5af058b66f8cbf3ca9398
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-13 13:45:52 +0300

    add repo that extends jpa repo for savings in db

--- ProviderService/src/main/java/com/nasser/providerservice/Repository/RequestPayloadRepository.java
+++ ProviderService/src/main/java/com/nasser/providerservice/Repository/RequestPayloadRepository.java
@@ -1,5 +1,9 @@
 package com.nasser.providerservice.Repository;
 
-public class RequestPayloadRepository {
+import com.nasser.providerservice.Model.RequestPayload;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface RequestPayloadRepository extends JpaRepository<RequestPayload, Long> {
 }
