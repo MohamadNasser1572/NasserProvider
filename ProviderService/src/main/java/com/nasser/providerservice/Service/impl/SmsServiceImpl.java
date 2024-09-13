2914308ce24abdf86068998b1a98ac8fb69ea447
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-13 23:56:38 +0300

    implement the logic of the sms service

--- ProviderService/src/main/java/com/nasser/providerservice/Service/impl/SmsServiceImpl.java
+++ ProviderService/src/main/java/com/nasser/providerservice/Service/impl/SmsServiceImpl.java
@@ -1,2 +1,23 @@
-package com.nasser.providerservice.Service.impl;public class SmsServiceImpl {
+package com.nasser.providerservice.Service.impl;
+
+import com.nasser.providerservice.DTO.SmsRequestDTO;
+import com.nasser.providerservice.Service.SmsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class SmsServiceImpl implements SmsService {
+
+    private final String provider2URL = "http://localhost:8082/POSTprovider2";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Override
+    public String sendSms(SmsRequestDTO smsRequest) {
+        // Send request to provider 2
+        String response = restTemplate.postForObject(provider2URL, smsRequest, String.class);
+        return "Provider 2 Response: " + response;
+    }
 }
