5ad7423317840975905dc6fddfe144340dd8d379
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-13 14:09:48 +0300

    add service implementation that shows the logic of the service interface methods

--- ProviderService/src/main/java/com/nasser/providerservice/Service/impl/RequestPayloadServiceImpl.java
+++ ProviderService/src/main/java/com/nasser/providerservice/Service/impl/RequestPayloadServiceImpl.java
@@ -1,2 +1,36 @@
-package com.nasser.providerservice.Service.impl;public class RequestPayloadServiceImpl {
+package com.nasser.providerservice.Service.impl;
+
+import com.nasser.providerservice.Model.RequestPayload;
+import com.nasser.providerservice.Repository.RequestPayloadRepository;
+import com.nasser.providerservice.Service.RequestPayloadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RequestPayloadServiceImpl implements RequestPayloadService {
+
+    @Autowired
+    private RequestPayloadRepository requestPayloadRepository;
+
+    @Override
+    public RequestPayload create(RequestPayload payload) {
+        return requestPayloadRepository.save(payload);
+    }
+
+    @Override
+    public RequestPayload findById(Long id) {
+        return requestPayloadRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<RequestPayload> findAll() {
+        return requestPayloadRepository.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        requestPayloadRepository.deleteById(id);
+    }
 }
