6017c7e4f8112d5dc2964715ac31aedc7b167884
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-13 13:45:11 +0300

    add Entity and Table annotations

--- ProviderService/src/main/java/com/nasser/providerservice/Model/RequestPayload.java
+++ ProviderService/src/main/java/com/nasser/providerservice/Model/RequestPayload.java
@@ -1,9 +1,7 @@
 package com.nasser.providerservice.Model;
 
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +12,8 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Entity
+@Table
 public class RequestPayload {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
