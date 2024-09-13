83f38bac27183ea673ca7dcc99d575ea489a969b
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-13 14:44:15 +0300

    write the business logic of each method to use them in controller

--- ProviderService/src/main/java/com/nasser/providerservice/Service/impl/RequestPayloadServiceImpl.java
+++ ProviderService/src/main/java/com/nasser/providerservice/Service/impl/RequestPayloadServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RequestPayloadServiceImpl implements RequestPayloadService {
@@ -30,7 +31,28 @@ public class RequestPayloadServiceImpl implements RequestPayloadService {
     }
 
     @Override
-    public void deleteById(Long id) {
-        requestPayloadRepository.deleteById(id);
+    public boolean deleteById(Long id) {
+        try {
+            requestPayloadRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
+
+    @Override
+    public boolean updateRequestPayload(Long id, RequestPayload updatedPayload) {
+        Optional<RequestPayload> payloadOptional = requestPayloadRepository.findById(id);
+        if (payloadOptional.isPresent()) {
+            RequestPayload payload = payloadOptional.get();
+            payload.setPhoneNumber(updatedPayload.getPhoneNumber());
+            payload.setMessage(updatedPayload.getMessage());
+            payload.setLanguage(updatedPayload.getLanguage());
+            requestPayloadRepository.save(payload);
+            return true;
+        }
+        return false;
+    }
+
+
 }
