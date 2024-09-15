7bd19e8233267b59f4696adae20b0c05e2f27de6
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-15 21:09:51 +0300

    add controller for provider2

--- ProviderService/src/main/java/com/nasser/providerservice/Controller/Provider2Controller.java
+++ ProviderService/src/main/java/com/nasser/providerservice/Controller/Provider2Controller.java
@@ -1,2 +1,34 @@
-package com.nasser.providerservice.Controller;public class Provider2Controller {
+package com.nasser.providerservice.Controller;
+
+import com.nasser.providerservice.Entity.SMS;
+import com.nasser.providerservice.Service.ISMSProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/provider2/sms")
+public class Provider2Controller {
+
+    private final ISMSProvider smsProvider;
+
+    @Autowired
+    public Provider2Controller(@Qualifier("provider2Service") ISMSProvider smsProvider) {
+        this.smsProvider = smsProvider;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> sendSMS(@RequestBody SMS request) {
+        String result = smsProvider.sendSMS(request);
+        if ("true".equalsIgnoreCase(result)) {
+            return ResponseEntity.ok("SMS sent successfully via Provider2");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to send SMS via Provider2");
+        }
+    }
 }
