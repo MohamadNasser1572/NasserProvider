9ec74766eeca0151e312ce6e85f7dab873da43e6
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-15 15:31:45 +0300

    same for provider 2

--- ProviderService/src/main/java/com/nasser/providerservice/Service/impl/Provider2ServiceImpl.java
+++ ProviderService/src/main/java/com/nasser/providerservice/Service/impl/Provider2ServiceImpl.java
@@ -2,28 +2,23 @@ package com.nasser.providerservice.Service.impl;
 
 import com.nasser.providerservice.Entity.SMS;
 import com.nasser.providerservice.Service.ISMSProvider;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 
 @Service
+@Qualifier("provider2Service")
 public class Provider2ServiceImpl implements ISMSProvider {
+
+    private final RestTemplate restTemplate;
+    private final String provider2Url = "http://localhost:8080/api/POSTprovider2";
+
+    public Provider2ServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Override
     public String sendSMS(SMS sms) {
-        final RestTemplate restTemplate = new RestTemplate();
-        final String providerUrl = "http://localhost:8082/Provider2";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<SMS> request = new HttpEntity<>(sms, headers);
-
-        //POST request
-        ResponseEntity<String> response = restTemplate.postForEntity(providerUrl + "/send", request, String.class);
-
-        System.out.println("Sending SMS for provider2");
-
-        return response.getBody();
+        return restTemplate.postForObject(provider2Url, sms, String.class);
     }
 }
