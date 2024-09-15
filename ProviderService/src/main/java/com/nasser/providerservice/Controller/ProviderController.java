4a18f4c4aecdee5ec4fe3455f71922ec85d2bb85
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-15 21:10:30 +0300

    comment out provider controller for better understanding

--- ProviderService/src/main/java/com/nasser/providerservice/Controller/ProviderController.java
+++ ProviderService/src/main/java/com/nasser/providerservice/Controller/ProviderController.java
@@ -1,24 +1,24 @@
-package com.nasser.providerservice.Controller;
-
-import com.nasser.providerservice.Entity.RequestPayload;
-import com.nasser.providerservice.Service.IProviderService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api")
-public class ProviderController {
-
-    @Autowired
-    private IProviderService IProviderService;
-
-    @GetMapping("/GETprovider1")
-    public String getProvider1(@RequestParam Long phoneNumber, @RequestParam String message) {
-        return IProviderService.getProvider1(phoneNumber, message);
-    }
-
-    @PostMapping("/POSTprovider2")
-    public String postProvider2(@RequestBody RequestPayload requestPayload) {
-        return IProviderService.postProvider2(requestPayload);
-    }
-}
+//package com.nasser.providerservice.Controller;
+//
+//import com.nasser.providerservice.Entity.RequestPayload;
+//import com.nasser.providerservice.Service.IProviderService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class ProviderController {
+//
+//    @Autowired
+//    private IProviderService IProviderService;
+//
+//    @GetMapping("/GETprovider1")
+//    public String getProvider1(@RequestParam Long phoneNumber, @RequestParam String message) {
+//        return IProviderService.getProvider1(phoneNumber, message);
+//    }
+//
+//    @PostMapping("/POSTprovider2")
+//    public String postProvider2(@RequestBody RequestPayload requestPayload) {
+//        return IProviderService.postProvider2(requestPayload);
+//    }
+//}
