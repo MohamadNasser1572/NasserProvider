170db982816044c0c68d962961c95f029ced9a13
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-15 17:02:12 +0300

    changed link port number

--- ProviderService/src/main/java/com/nasser/providerservice/Service/impl/Provider1ServiceImpl.java
+++ ProviderService/src/main/java/com/nasser/providerservice/Service/impl/Provider1ServiceImpl.java
@@ -9,9 +9,9 @@ import org.springframework.web.client.RestTemplate;
 @Service
 @Qualifier("provider1Service")
 public class Provider1ServiceImpl implements ISMSProvider {
-
+    //here we are putting the provider URL
     private final RestTemplate restTemplate;
-    private final String provider1Url = "http://localhost:8080/api/GETprovider1";
+    private final String provider1Url = "http://localhost:8081/GETprovider1/sms";
 
     public Provider1ServiceImpl(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
