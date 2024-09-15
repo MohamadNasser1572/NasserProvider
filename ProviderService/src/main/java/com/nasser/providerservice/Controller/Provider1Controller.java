2b12a9d9b3e995280d30fa399a8ff52d1a31aff8
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-15 21:09:16 +0300

    added controller class for provider 1 only

---
+++ ProviderService/src/main/java/com/nasser/providerservice/Controller/Provider1Controller.java
@@ -0,0 +1,36 @@
+package com.nasser.providerservice.Controller;
+
+import com.nasser.providerservice.Entity.SMS;
+import com.nasser.providerservice.Service.ISMSProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/provider1/sms")
+public class Provider1Controller {
+
+
+    private final ISMSProvider smsProvider;
+
+    @Autowired
+    public Provider1Controller(@Qualifier("provider1Service") ISMSProvider smsProvider) {
+        this.smsProvider = smsProvider;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> sendSMS(@RequestBody SMS request) {
+        boolean result = Boolean.parseBoolean(smsProvider.sendSMS(request));
+        if (result) {
+            return ResponseEntity.ok("SMS sent successfully via Provider1");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to send SMS via Provider1");
+        }
+    }
+}
+
