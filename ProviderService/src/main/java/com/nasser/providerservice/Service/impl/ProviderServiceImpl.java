5bce318f16fe56eb34b0e46d457fe914f6d4ab47
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-16 22:48:08 +0300

    change port number of GETprovider1 to 8082

--- ProviderService/src/main/java/com/nasser/providerservice/Service/impl/ProviderServiceImpl.java
+++ ProviderService/src/main/java/com/nasser/providerservice/Service/impl/ProviderServiceImpl.java
@@ -24,7 +24,7 @@ public class ProviderServiceImpl implements ProviderService {
 
     @Override
     public String getProvider1(Long phoneNumber, String message) {
-        String url = "http://localhost:8081/GETprovider1?phoneNumber=" + phoneNumber + "&message=" + message;
+        String url = "http://localhost:8082/GETprovider1?phoneNumber=" + phoneNumber + "&message=" + message;
         return restTemplate.getForObject(url, String.class);
     }
 
