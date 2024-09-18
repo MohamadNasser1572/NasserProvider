7cb9e964cd40e25579047a24fbc42b6eab878a38
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-18 11:09:14 +0300

    Add exception for random provider

---
+++ ProviderService/src/main/java/com/nasser/providerservice/Exception/RandomProviderException.java
@@ -0,0 +1,7 @@
+package com.nasser.providerservice.Exception;
+
+public class RandomProviderException extends RuntimeException {
+    public RandomProviderException(String message) {
+        super(message);
+    }
+}
