0d01606f7868f81c8090cf4c50e3175300893345
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-25 16:18:30 +0300

    consume messages from queue1 using listener

--- Provider2/src/main/java/com/nasser/provider2/Service/Queue1Listener.java
+++ Provider2/src/main/java/com/nasser/provider2/Service/Queue1Listener.java
@@ -1,2 +1,15 @@
-package com.nasser.provider2.Service;public class Queue1Listener {
+package com.nasser.provider2.Service;
+
+import com.nasser.provider2.RequestPayload;
+import com.nasser.provider2.Config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Queue1Listener {
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_1)
+    public void consumeMessageFromQueue1(RequestPayload payload) {
+        System.out.println("Message consumed from queue1: " + payload.getMessage());
+    }
 }
