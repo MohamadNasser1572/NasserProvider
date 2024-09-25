8da889931d1cd19fc7e149e3e5ec417247f05c79
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-25 16:19:21 +0300

    consume messages from queue2 using a cron job

--- Provider2/src/main/java/com/nasser/provider2/Service/Queue2CronConsumer.java
+++ Provider2/src/main/java/com/nasser/provider2/Service/Queue2CronConsumer.java
@@ -1,2 +1,25 @@
-package com.nasser.provider2.Service;public class Queue2CronConsumer {
+package com.nasser.provider2.Service;
+
+import com.nasser.provider2.Config.RabbitMQConfig;
+import com.nasser.provider2.RequestPayload;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Queue2CronConsumer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public Queue2CronConsumer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Scheduled(fixedRate = 10000)  //every 10 seconds
+    public void consumeMessageFromQueue2() {
+        RequestPayload payload = (RequestPayload) rabbitTemplate.receiveAndConvert(RabbitMQConfig.QUEUE_2);
+        if (payload != null) {
+            System.out.println("Message consumed from queue2: " + payload.getMessage());
+        }
+    }
 }
