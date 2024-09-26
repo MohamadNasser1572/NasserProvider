cc892d2bd5becefc0944187893bd402c4c9aec1f
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-26 10:07:39 +0300

    add message converter to json format using jackson

--- Provider2/src/main/java/com/nasser/provider2/Config/RabbitMQConfig.java
+++ Provider2/src/main/java/com/nasser/provider2/Config/RabbitMQConfig.java
@@ -1,6 +1,11 @@
 package com.nasser.provider2.Config;
 
+
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,4 +23,17 @@ public class RabbitMQConfig {
     public Queue queue2() {
         return new Queue(QUEUE_2);
     }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+
 }
