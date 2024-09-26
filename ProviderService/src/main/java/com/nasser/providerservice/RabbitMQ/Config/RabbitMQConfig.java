903fb03f091e21f47a62633afb925b2df7f020c1
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-26 10:12:05 +0300

    Configure RabbitMQ with JSON message conversion and type mapping

--- ProviderService/src/main/java/com/nasser/providerservice/RabbitMQ/Config/RabbitMQConfig.java
+++ ProviderService/src/main/java/com/nasser/providerservice/RabbitMQ/Config/RabbitMQConfig.java
@@ -1,23 +1,45 @@
 package com.nasser.providerservice.RabbitMQ.Config;
 
-import org.springframework.amqp.core.Queue;
+import com.nasser.providerservice.RequestPayload.Entity.RequestPayload;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.support.converter.DefaultClassMapper;
 
+import java.util.HashMap;
+import java.util.Map;
 
+@EnableRabbit
 @Configuration
 public class RabbitMQConfig {
     public static final String QUEUE_1 = "queue1";
     public static final String QUEUE_2 = "queue2";
 
     @Bean
-    public Queue queue1() {
-        return new Queue(QUEUE_1);
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+        return rabbitTemplate;
     }
 
     @Bean
-    public Queue queue2() {
-        return new Queue(QUEUE_2);
+    public DefaultClassMapper typeMapper() {
+        DefaultClassMapper classMapper = new DefaultClassMapper();
+        Map<String, Class<?>> idClassMapping = new HashMap<>();
+        idClassMapping.put("com.nasser.providerservice.RequestPayload.Entity.RequestPayload", RequestPayload.class);
+        classMapper.setIdClassMapping(idClassMapping);
+        return classMapper;
+    }
+
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        converter.setClassMapper(typeMapper());
+        return converter;
     }
 }
 
